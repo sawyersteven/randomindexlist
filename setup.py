567ffe0bbccb923b184332bc2b4458a470423509
sawyersteven <sawyerstevenk@gmail.com>
2018-06-30 12:02:05 -0400

    Correct setup for pypi description formatting

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import setuptools
+from m2r import convert
 
 with open("README.md", "r") as f:
-    long_description = f.read()
+    long_description = convert(f.read())
 
 setuptools.setup(
     name="RandomIndexList",
-    version="1.3",
+    version="0.3",
     author="SawyerSteven",
     author_email="sawyerstevenk@gmail.com",
     description="Start lists at any position. Because you can!",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/sawyersteven/randomindexlist",
     packages=setuptools.find_packages(),
     classifiers=(
